7294a5c45503cfb7165f7654fafe113614506065
alexbft <alexbft@users.noreply.github.com>
2024-08-31 16:43:26 +0200

    style img to fit

--- src/components/PageView/PageView.tsx
+++ src/components/PageView/PageView.tsx
@@ -1,3 +1,9 @@
 export function PageView({ imageUrl, onClick }: { imageUrl: string, onClick: () => void }) {
-  return <img src={imageUrl} onClick={onClick} />
+  return (
+    <img
+      className="absolute top-0 left-0 h-dvh w-dvw object-contain"
+      src={imageUrl}
+      onClick={onClick}
+    />
+  );
 }
