b76b7386fbd090728f54261ce248643dd1d5f792
alexbft <alexbft@users.noreply.github.com>
2024-09-08 00:03:13 +0200

    move close button to the left

--- src/components/HudCloseButton/HudCloseButton.tsx
+++ src/components/HudCloseButton/HudCloseButton.tsx
@@ -4,7 +4,7 @@ import { Button } from "../ui/button";
 export function HudCloseButton({ onClick }: { onClick: () => void }) {
   return (
     <Button
-      className="absolute top-4 right-4 group size-16 hover:bg-black hover:bg-opacity-25 rounded-full"
+      className="absolute top-4 right-6 group size-16 hover:bg-black hover:bg-opacity-25 rounded-full"
       variant="ghost"
       onClick={onClick}>
       <Cross2Icon className="text-white opacity-30 group-hover:opacity-100 size-8" />
