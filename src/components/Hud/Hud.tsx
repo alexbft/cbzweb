05cbb0d75a67f84fc7774996f82d9438fa384744
alexbft <alexbft@users.noreply.github.com>
2024-10-12 18:21:08 +0200

    try again

--- src/components/Hud/Hud.tsx
+++ src/components/Hud/Hud.tsx
@@ -9,9 +9,10 @@ export function Hud({ hidden, children }: HudProps) {
 	return (
 		<div
 			className={cn(
-				"pointer-events-none fixed top-0 left-0 z-[1] h-full w-full transition-transform duration-500 *:pointer-events-auto",
+				"pointer-events-none fixed top-0 left-0 z-[1] h-full w-full touch-none transition-transform duration-500 *:pointer-events-auto",
 				hidden && "-translate-y-80 transform",
 			)}
+			onTouchStart={(e) => e.preventDefault()}
 		>
 			{children}
 		</div>
